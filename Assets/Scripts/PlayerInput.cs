1e69ffdc02f4c646c02a28bfd31b965e16cb7e54
MILKSPC\mikay <mikaylahub@gmail.com>
2026-04-09 19:49:07 -0400

    Add Animation for walking, made game UI

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,10 +5,12 @@ public class PlayerInput : MonoBehaviour
 {
     public float speed = 5;
     public Vector2 movement;
+    private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {  
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,7 +21,20 @@ public class PlayerInput : MonoBehaviour
 
     public void MovePlayer(InputAction.CallbackContext context)
     {
+        animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", movement.x);
+            animator.SetFloat("LastInputY", movement.y);
+        }
+
         //updates the movement when the InputAction is called based on the WASD inputs
         movement = context.ReadValue<Vector2>();
+
+        animator.SetFloat("InputX", movement.x);
+        animator.SetFloat("InputY", movement.y);
+
     }
 }
